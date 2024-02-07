00826db4fdb82e0d1b6230004d9bbb5e9d5e1136
anthonyV52 <avelasquez11@miners.utep.edu>
2024-02-07 15:41:58 -0700

    working font and arrow

---
+++ .#draw_shapes.c
@@ -0,0 +1,1 @@
+student@systems-vm.2121:1707341275
--- draw_chars.c
+++ draw_chars.c
@@ -8,7 +8,19 @@ void print_char_11x16(char c)
     unsigned short rowBits = font_11x16[c][row];
     for (char col = 0; col < 16; col++) {
       unsigned short colMask = 1 << (15-col); /* mask to select bit associated with bit */
-      putchar( (rowBits & colMask) ? '*' : ' ');
+      putchar( (rowBits & colMask) ? '*' : '-');
+    }
+    putchar('\n');
+  }
+}
+
+void print_char_5x7(char c){
+  c -= 0x20;
+  for (char row = 0; row < 5; row++){
+    unsigned short rowBits = font_5x7[c][row];
+    for (char col = 0; col < 7; col++) {
+      unsigned short colMask = 1 << (6-col);
+      putchar( (rowBits & colMask) ? '*' : '-');
     }
     putchar('\n');
   }
